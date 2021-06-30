804e6381e99fefe867b77dfa7c1cd6c65ad567f5
Henry <hpelaez@solutionsmedias360.com>
2021-06-29 21:04:23 -0400

    Update project
    Create Contact

--- src/App.js
+++ src/App.js
@@ -1,13 +1,73 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts';
+import PropTypes from 'prop-types';
+import * as ContactsAPI from './utils/ContactsAPI';
+import CreateContact from './CreateContact';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
+  state = {
+    contacts: [],
+    screen: 'list'
+  }
+
+  componentDidMount() {
+    ContactsAPI.getAll()
+      .then((contacts) => {
+        this.setState(() => ({
+          contacts
+        }))
+      })
+  }
+
+  removeContact = (contact) => {
+    this.setState(currentState =>({
+      contacts: currentState.contacts.filter((c) => {
+        return c.id !== contact.id
+      })
+    }))
+    ContactsAPI.remove(contact)
+  }
+
+  createContact = (contact) => {
+    ContactsAPI.create(contact)
+      .then((contact) => {
+        this.setState((currentState) => ({
+          contacts: currentState.contacts.concat([contact])
+        }))
+      })
+  }
+
   render() {
     return (
       <div>
-        Hello World
+        <Route exact path='/' render={() => (
+          <ListContacts 
+            contacts={this.state.contacts} 
+            onDeleteContact={this.removeContact} 
+            onNavigate={() => {
+              this.setState(() => ({
+                screen: 'create'
+              }))
+            }} 
+          /> 
+        )} />
+        <Route path='/create' render={({ history }) => (
+          <CreateContact 
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+          />
+        )} />
       </div>
     );
   }
+}
+
+ListContacts.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  onDeleteContact: PropTypes.func.isRequired,
 }
 
 export default App;
